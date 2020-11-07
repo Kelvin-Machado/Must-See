27fa03e965d99233c1d66edd889214521971a31f
Kelvin Batista Machado <kelvinmachado@live.com>
2020-11-06 21:41:37 -0300

    Configura view de resultado do filme pesquisado

--- Must-See/Controller/SearchViewController.swift
+++ Must-See/Controller/SearchViewController.swift
@@ -9,10 +9,21 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var movieView: UIView!
+    @IBOutlet weak var posterImg: UIImageView!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var yearLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureNavigationBar(largeTitleColor: .white, backgoundColor: .purple, tintColor: .white, title: "Must-see", preferredLargeTitle: true)
+        
+        searchBar.searchTextField.backgroundColor = .white
+        movieView.layer.cornerRadius = 10
+        posterImg.layer.cornerRadius = 5
         
         view.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
     }
