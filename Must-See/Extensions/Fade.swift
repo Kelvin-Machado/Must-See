2e21bd8899895b999eb9603fbcd8046e1397c97d
Kelvin Batista Machado <kelvinmachado@live.com>
2020-11-16 16:00:54 -0300

    Altera a função fade in

--- Must-See/Extensions/Fade.swift
+++ Must-See/Extensions/Fade.swift
@@ -9,17 +9,16 @@ import UIKit
 
 extension UIView {
     
-    func fadeIn(duration: TimeInterval = 0.5, delay: TimeInterval = 0.0, completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in }) {
+    func fadeIn() {
         self.alpha = 0.0
         
-        UIView.animate(withDuration: duration, delay: delay, options: UIView.AnimationOptions.curveEaseIn, animations: {
+        UIView.animate(withDuration: 0.5) {
             self.alpha = 1.0
-        }, completion: completion)
-        
+        }
     }
     
     func fadeOut() {
-        UIView.animate(withDuration: 0.5) {
+        UIView.animate(withDuration: 0.3) {
             self.alpha = 0.0
         }
     }
