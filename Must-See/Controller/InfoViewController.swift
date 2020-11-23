899159fa7c6d7c5758a4250f1760aacca8f91d5c
Kelvin Batista Machado <kelvinmachado@live.com>
2020-11-23 20:05:05 -0300

    Corrige o layout da tela de informações

--- Must-See/Controller/InfoViewController.swift
+++ Must-See/Controller/InfoViewController.swift
@@ -19,10 +19,23 @@ class InfoViewController: UIViewController {
     var viewPoster: UIView?
     var imageView: UIImageView?
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    
+        infoTxtView.isScrollEnabled = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tituloLbl.text = "\(infoView!.title) (\(infoView!.year))"
+        sinopseConfig()
+        notasConfig()
+        infoConfig()
+        
+    }
+    
+    func sinopseConfig() {
         
         let path = UIBezierPath(rect: CGRect(x: 0, y: 0, width: 150, height: 200))
         sinopseTxtView.textContainer.exclusionPaths = [path]
@@ -30,30 +43,43 @@ class InfoViewController: UIViewController {
         imageView = UIImageView(image: poster!.resize(380))
         
         sinopseTxtView.addSubview(imageView!)
-        
         sinopseTxtView.text = infoView?.plot
-        sinopseTxtView.isSelectable = false
-        sinopseTxtView.isEditable = false
-        infoTxtView.isSelectable = false
-        infoTxtView.isEditable = false
+        sinopseTxtView.layer.cornerRadius = 5
         
+    }
+    
+    func notasConfig() {
+    
         let notas = infoView!.ratings.count
         
         if notas != 0 {
             notasLbl.text = "\n"
             for source in 0...notas-1 {
-                notasLbl.text?.append(contentsOf: " \(infoView!.ratings[source].source): \(infoView!.ratings[source].value) \n")
+                notasLbl.text?.append(contentsOf: "   \(infoView!.ratings[source].source): \(infoView!.ratings[source].value) \n")
             }
         } else {
             print("Sem avaliações" )
         }
         
-        infoTxtView.text = " Director: \(infoView!.director!)\n Runtime: \(infoView!.runtime!)\n Genre: \(infoView!.genre!) \n"
-        
-        sinopseTxtView.layer.cornerRadius = 5
         notasLbl.clipsToBounds = true
         notasLbl.layer.cornerRadius = 5
-        infoTxtView.layer.cornerRadius = 5
         
     }
+    
+    func infoConfig() {
+        
+        infoTxtView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            infoTxtView.topAnchor.constraint(equalTo: notasLbl.bottomAnchor, constant: 8),
+            infoTxtView.heightAnchor.constraint(lessThanOrEqualToConstant: 200),
+            infoTxtView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12),
+            infoTxtView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12),
+            infoTxtView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -12)
+        ])
+        
+        infoTxtView.layer.cornerRadius = 5
+        infoTxtView.text = " Director: \(infoView!.director!)\n\n Runtime: \(infoView!.runtime!)\n\n Genre: \(infoView!.genre!) \n"
+    }
 }
+
