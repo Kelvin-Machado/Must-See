712e487c5ddfe45ecac11058641d47d18b0f17e5
Kelvin Batista Machado <kelvinmachado@live.com>
2020-12-04 20:14:42 -0300

    Adiciona célula para lista de filmes

--- Must-See/Controller/FilmesTableViewController.swift
+++ Must-See/Controller/FilmesTableViewController.swift
@@ -6,85 +6,72 @@
 //
 
 import UIKit
+import RealmSwift
 
 class FilmesTableViewController: UITableViewController {
-
+    
+    let realm = try! Realm()
+    var filmes: Results<Filme>?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.title = "Lista de filmes"
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        loadCategories()
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-//         self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        if filmes!.count == 0 {
+            return 1
+        } else {
+            return  filmes!.count
+        }
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        
+        
+        let mySwitch = UISwitch()
+        mySwitch.addTarget(self, action: #selector(didChangeSwitch), for: .valueChanged)
+        cell.accessoryView = mySwitch
+        
+        
+        if filmes!.count == 0 {
+            cell.textLabel?.text = "Adicione Filmes"
+            mySwitch.isHidden = true
+        } else {
+            cell.textLabel?.text = filmes![indexPath.row].title
+            if filmes![indexPath.row].watched {
+                mySwitch.isOn = true
+            } else {
+                mySwitch.isOn = false
+            }
+        }
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    @objc func didChangeSwitch(_ sender: UISwitch) {
+        if sender.isOn {
+            print("Is On")
+        } else {
+            print("Is off")
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    func loadCategories() {
+        filmes = realm.objects(Filme.self)
+        tableView.reloadData()
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
